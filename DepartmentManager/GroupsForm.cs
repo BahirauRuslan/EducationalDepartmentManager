664414a231cb39570755cc6b02cbbfd444a720f7
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-09 19:59:47 +0300

    Add form for groups table

---
+++ DepartmentManager/GroupsForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DepartmentManager
+{
+    public partial class GroupsForm : Form
+    {
+        public GroupsForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PlansBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.plansBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.educationalDepartmentDataSet);
+        }
+
+        private void GroupsForm_Load(object sender, EventArgs e)
+        {
+            this.groupsTableAdapter.Fill(this.educationalDepartmentDataSet.Groups);
+            this.plansTableAdapter.Fill(this.educationalDepartmentDataSet.Plans);
+        }
+    }
+}
