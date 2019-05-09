ca408a9027e0b0e6434e5226952828caa1fed639
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-09 20:33:57 +0300

    Update main program class

--- DepartmentManager/Program.cs
+++ DepartmentManager/Program.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace DepartmentManager
 {
     public static class Program
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         public static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainFrom());
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainFrom());
+            }
+            catch (Exception e)
+            {
+                Program.Logger.Error(e.Message);
+                Program.Logger.Error(e.StackTrace);
+                MessageBox.Show(e.Message, "Application error");
+            }
         }
     }
 }
