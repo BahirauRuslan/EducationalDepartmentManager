55628cdef6d43eda5cb59fe8244db35bba38fedf
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-09 17:57:19 +0300

    Add form for subjects table

---
+++ DepartmentManager/SubjectsForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DepartmentManager
+{
+    public partial class SubjectsForm : Form
+    {
+        public SubjectsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SubjectsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.subjectsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.educationalDepartmentDataSet);
+
+        }
+
+        private void SubjectsForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'educationalDepartmentDataSet.Subjects' table. You can move, or remove it, as needed.
+            this.subjectsTableAdapter.Fill(this.educationalDepartmentDataSet.Subjects);
+
+        }
+    }
+}
